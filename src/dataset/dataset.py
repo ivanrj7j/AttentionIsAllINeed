9f187c1ca962f16730e0c98b24572c6b2d205c20
ivanrj7j <ivanrj7j@gmail.com>
2026-05-03 01:12:24 +0530

    working on dataset

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -16,8 +16,24 @@ class TransformerDataset(IterableDataset):
 
         self.srcTokenizer = srcTokenizer
         self.tgtTokenizer = tgtTokenizer
-        
-    # def processBatch
+
+    def tokenizeString(self, string:str|list[str], tokenizer:PreTrainedTokenizerFast):
+        return tokenizer(
+            string,
+            max_length=self.maxSentenceSize, 
+            padding='max_length', 
+            truncation=True, 
+            add_special_tokens=True
+        )['input_ids']
+    
+    def processBatch(self, src:list[str], tgt:list[str]):
+        assert len(src) == len(tgt)
+
+        srcTokens = self.tokenizeString(src, self.srcTokenizer)
+        tgtTokens = self.tokenizeString(tgt, self.tgtTokenizer)
+
+        for a, b in zip(srcTokens, tgtTokens):
+            yield (torch.tensor(a), torch.tensor(b))
     
     def __iter__(self) -> Iterator:
         return zip('a', 'b')
@@ -28,14 +44,23 @@ if __name__ == "__main__":
     srcFile = '../../dataset/engTest.txt'
     tgtFile = '../../dataset/malTest.txt'
 
-    srcTokenizer = PreTrainedTokenizerFast(tokenizer_file='../../dataset/engTokenizer.json')
-    tgtTokenizer = PreTrainedTokenizerFast(tokenizer_file='../../dataset/malTokenizer.json')
-
-    srcTokenizer.post
+    srcTokenizer = PreTrainedTokenizerFast(tokenizer_file='../../dataset/engTokenizer.json',  
+                                 bos_token="<bos>", 
+                                 eos_token="<eos>",
+                                 pad_token="<pad>",
+                                 unk_token="<unk>")
+    tgtTokenizer = PreTrainedTokenizerFast(tokenizer_file='../../dataset/malTokenizer.json', 
+                                 bos_token="<bos>", 
+                                 eos_token="<eos>",
+                                 pad_token="<pad>",
+                                 unk_token="<unk>")
 
     dataset = TransformerDataset(srcFile, tgtFile, 64 + 2, 1000, srcTokenizer, tgtTokenizer)
 
 
-    with open(srcFile, 'r', encoding='utf-8') as sF, \
-        open(tgtFile, 'r', encoding='utf-8') as tF:
-        pass
+    testSrc = ['Hello world!', "I love you!"]
+    testTgt = ['ഹലോ വേൾഡ്!', 'ഞാൻ നിന്നെ സ്നേഹിക്കുന്നു']
+
+    for x, y in dataset.processBatch(testSrc, testTgt):
+        print("x:", x)
+        print("y:", y)
